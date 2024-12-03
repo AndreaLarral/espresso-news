a3e5720a8ddf332c2fababc73b6798027c7aa364
AndreaLarral <you@example.com>
2024-12-03 13:54:38 +0100

    testing the working branch

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <h1 class="text-8xl font-black text-emerald-600">{{ title }}</h1>
     <h2 class="text-4xl font-black text-pink-400">Bon dia!</h2>
+    <h3 class="text-2xl font-black text-pink-200">Test_Change</h3>
 
     <RouterView/>
   </div>
