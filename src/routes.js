dd2114e8860611eea4a5756cdd3e5aa8dd685a82
AndreaLarral <you@example.com>
2024-12-03 14:10:13 +0100

    not found view

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
 import { createWebHashHistory, createRouter } from 'vue-router'
 import Home from './views/Home.vue'
+import NotFound from "./views/NotFound.vue"
 
 const routes = [
     {
         path: "/",
         name: "Home",
         component: Home
+    },
+    {
+        path: "/:path(.*)",
+        name: "Not found",
+        component: NotFound
     }
     // Aquí pondrás las siguientes rutas
 ];
