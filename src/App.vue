b9f44acda8d7600871de2831f22d20c9d9455166
AndreaLarral <you@example.com>
2024-12-03 13:31:34 +0100

    instalación del router

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+    <h1 class="text-8xl font-black text-emerald-600">{{ title }}</h1>
+    <h2 class="text-4xl font-black text-pink-400">Bon dia!</h2>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+    <RouterView/>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  data(){
+    return{
+      title: "Hola❤️"
+    }
+  }
+  
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+</script>
+<style>
+  
 </style>
